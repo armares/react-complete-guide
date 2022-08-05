f6571ede009d5c66d1c0f1d796af2945263d6cd7
armares <armares2001@libero.it>
2022-08-05 19:56:36 +0200

    finish project

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -2,10 +2,14 @@ import React,{useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
+    const ELadd=<button type='submit'>Add Expense</button>;
+    const Elcancel=<button type='submit' onClick={cancelHandler}>Cancel</button>;
+    const ELaddNew=<button type='submit' onClick={addNewHandler}>Add New Expense</button>
+
+    
     const [errorTitle,changeTitleErr]=useState('');
     const [errorPrice,changePriceErr]=useState('');
     const [errorDate,changeDateErr]=useState('');
-    const [d_none,changeVisDiv]=useState('');
     // const [userImput,setUserImput]=useState({
     //     enteredTitle:'',
     //     enteredPrice:'',
@@ -14,8 +18,35 @@ const ExpenseForm = (props) => {
     const [enteredTitle,setEnteredTitle]=useState('');
     const [enteredPrice,setEnteredPrice]=useState('');
     const [enteredDate,setEnteredDate]=useState('');
+
+    const [addNew,setAddNew]=useState('');
+    // let addNew=<button type='submit'>Add New Expense</button>;
+    const [add,setAdd]=useState(ELadd);
+    const [cancel,setCancel]=useState(Elcancel);
     
-    const titleChangeHandler=(e)=>{
+    const ELtitle=(
+        <div className='new-expense__control'>
+            <label>Title</label>
+            <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
+        </div>
+    )
+    const ELprice=(
+        <div className='new-expense__control'>
+            <label>Price</label>
+            <input type="number" min="0.01" step="0.01" onChange={priceChangeHandler} value={enteredPrice}/>
+        </div>
+    )
+    const ELdate=(
+        <div className='new-expense__control'>
+                <label>Date</label>
+                <input type="date" min="2016-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate}/>
+        </div>
+    )
+    const [controlTitle,setControlTitle]=useState(ELtitle);
+    const [controlPrice,setControlPrice]=useState(ELprice);
+    const [controlDate,setControlDate]=useState(ELdate);
+    
+    function titleChangeHandler(e){
         // console.log(e.target.value);
         const e_v=e.target.value;
         // setUserImput({
@@ -26,15 +57,8 @@ const ExpenseForm = (props) => {
         //     return {...prevState,enteredTitle:e_v};
         // });
         setEnteredTitle(e_v);
-        if (e_v.length>4) {
-            // changeVisDiv('');
-            changeTitleErr('c\'è un errore');
-        }else if (e_v.length<=4) {
-            // changeVisDiv('d-none');
-            changeTitleErr('');
-        }
     }
-    const priceChangeHandler=(e)=>{
+    function priceChangeHandler(e){
         // console.log(e.target.value);
         const e_v=e.target.value;
         // setUserImput({
@@ -45,15 +69,8 @@ const ExpenseForm = (props) => {
         //     return {...prevState,enteredPrice:e_v};
         // });
         setEnteredPrice(e_v);
-        if (e_v.length>4) {
-            // changeVisDiv('');
-            changePriceErr('c\'è un errore');
-        }else if (e_v.length<=4) {
-            // changeVisDiv('d-none');
-            changePriceErr('');
-        }
     }
-    const dateChangeHandler=(e)=>{
+    function dateChangeHandler(e){
         // console.log(e.target.value);
         const e_v=e.target.value;
         console.log(e_v);
@@ -65,16 +82,6 @@ const ExpenseForm = (props) => {
         //     return {...prevState,enteredDate:e_v};
         // });
         setEnteredDate(e_v);
-        const notYears=['2022','2021','2020'];
-        // if (e_v.contains(...notYears)) {
-        //     // changeVisDiv('');
-        //     changeDateErr('c\'è un errore');
-        // }else if (e_v.length<=4) {
-        //     // changeVisDiv('d-none');
-        //     changeDateErr('');
-        // }
-        
-        // console.log(notYears.splice(3,9));
     }
     const submitHandler=(e)=>{
         e.preventDefault();
@@ -87,31 +94,39 @@ const ExpenseForm = (props) => {
         // console.log(expenseData);
         props.onSaveExpenseData(expenseData);
     }
-
-    // console.log(userImput.enteredTitle);
-    // console.log(userImput.enteredPrice);
-    // console.log(userImput.enteredDate);
+    
+    function cancelHandler(e){
+        e.preventDefault();
+        setAdd('')
+        setCancel('')
+        setAddNew(ELaddNew)
+        setControlTitle('')
+        setControlPrice('')
+        setControlDate('')
+    }
+    function addNewHandler(e){
+        e.preventDefault();
+        setAdd(ELadd);
+        setCancel(Elcancel);
+        setAddNew('');
+        setControlTitle(ELtitle);
+        setControlPrice(ELprice);
+        setControlDate(ELdate);
+    }
+    
+    
+    
   return (
     <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-                <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
-                <div className={d_none}>{errorTitle}</div>
-            </div>
-            <div className='new-expense__control'>
-                <label>Price</label>
-                <input type="number" min="0.01" step="0.01" onChange={priceChangeHandler} value={enteredPrice}/>
-                <div className={d_none}>{errorPrice}</div>
-            </div>
-            <div className='new-expense__control'>
-                <label>Date</label>
-                <input type="date" min="2016-01-01" max="2022-12-31" onChange={dateChangeHandler} value={enteredDate}/>
-                <div className={d_none}>{errorDate}</div>
-            </div>
+            {controlTitle}
+            {controlPrice}
+            {controlDate}
         </div>
         <div className='new-expense__actions'>
-            <button type='submit'>Add Expense</button>
+            {cancel}
+            {addNew}
+            {add}
         </div>
     </form>
   )
